8ca4f08e6411cb602ee1a909b1c1fbdb207fbfd3
@a.shajdullin <a.shajdullin@rko-group.ru>
2026-02-06 11:16:26 +0500

    create database declarative base model and session maker

---
+++ backend/src/constants/models.py
@@ -0,0 +1,4 @@
+from typing import Final
+
+MAX_CHAR_FIELD: Final[int] = 256
+MAX_TEXT_FIELD: Final[int] = 5_000
---
+++ backend/src/database/orm.py
@@ -0,0 +1,29 @@
+import datetime
+from typing import Annotated
+from sqlalchemy import String, text
+from constants.models import MAX_CHAR_FIELD, MAX_TEXT_FIELD
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+max_char_field = Annotated[str, MAX_CHAR_FIELD]
+max_text_field = Annotated[str, MAX_TEXT_FIELD]
+
+class Base(DeclarativeBase):
+
+    type_annotation_map = {
+        max_char_field: String(MAX_CHAR_FIELD),
+        max_text_field: String(MAX_TEXT_FIELD),
+    }
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    created_at: Annotated[
+        datetime.datetime, 
+        mapped_column(server_default=text("TIMEZONE('utc', now())"))
+    ]
+    
+    updated_at: Annotated[
+        datetime.datetime, 
+        mapped_column(
+            server_default=text("TIMEZONE('utc', now())"),
+            onupdate=datetime.datetime.utcnow,
+        )]
