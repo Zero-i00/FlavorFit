14fd8b5ab44fbd1c141e94bda704aae5518c725e
@a.shajdullin <a.shajdullin@rko-group.ru>
2026-02-06 12:34:46 +0500

    create user models; connect migrations

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-BASE_DIR = Path(__file__).parent.parent.parent
+BASE_DIR = Path(__file__).parent.parent
 
 class PostgresSettings(BaseSettings):
     postgres_host: str = 'localhost'
---
+++ backend/src/database/models/user/user.py
@@ -0,0 +1,72 @@
+from enum import Enum
+from typing import Optional
+from sqlalchemy import ForeignKey
+from database.orm import Base, max_char_field
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+class RoleEnum(Enum):
+    USER = 'USER'
+    ADMIN = 'ADMIN'
+
+class GenderEnum(Enum):
+    MALE = 'MALE'
+    FEMALE = 'FEMALE'
+
+
+class ActivityLevelEnum(Enum):
+    SEDENTARY = 'SEDENTARY'
+    LIGHT = 'LIGHT'
+    MODERATE = 'MODERATE'
+    ACTIVE = 'ACTIVE'
+    VERY_ACTIVE = 'VERY_ACTIVE'
+
+
+class NutritionGoalEnum(Enum):
+    WEIGHT_LOSS = 'WEIGHT_LOSS'
+    MAINTENANCE = 'MAINTENANCE'
+    MUSCLE_GAIN = 'MUSCLE_GAIN'
+
+
+class UserModel(Base):
+    __tablename__ = 'users'
+
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[bytes]
+
+    role: Mapped[RoleEnum] = mapped_column(default=RoleEnum.USER)
+
+    is_active: Mapped[bool] = mapped_column(default=True)
+
+    profile: Mapped[Optional['ProfileModel']] = relationship(back_populates="user")
+
+
+class ProfileModel(Base):
+    __tablename__ = 'profiles'
+
+    full_name: Mapped[max_char_field]
+
+    age: Mapped[Optional[int]]
+    bio: Mapped[Optional[max_char_field]]
+    gender: Mapped[Optional[GenderEnum]]
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True, index=True)
+    user: Mapped["UserModel"] = relationship(back_populates="profile")
+
+
+class BodyParameterModel(Base):
+    __tablename__ = 'body_parameters'
+
+    height_cm: Mapped[Optional[float]]
+    weight_kg: Mapped[Optional[float]]
+    goal_weight_kg: Mapped[Optional[float]]
+
+    arm_cm: Mapped[Optional[float]]
+    chest_cm: Mapped[Optional[float]]
+    waist_cm: Mapped[Optional[float]]
+    thigh_cm: Mapped[Optional[float]]
+
+    activity_level: Mapped[ActivityLevelEnum]
+    nutrition_goal: Mapped[NutritionGoalEnum]
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True, index=True)
+    user: Mapped["UserModel"] = relationship(back_populates="parameters")
