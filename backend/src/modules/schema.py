48c1b4c79673951b8b4b108f6ff4fb40d19c56c7
@a.shajdullin <a.shajdullin@rko-group.ru>
2026-02-10 21:44:01 +0500

    decompose graphql methods

--- backend/src/modules/schema.py
+++ backend/src/modules/schema.py
@@ -1,19 +1,32 @@
 import strawberry
-from strawberry.tools import merge_types
 
 from modules.user.resolver import UserQuery, UserMutation
 from modules.auth.resolver import AuthQuery, AuthMutation
 
-Query = merge_types("Query", (
-    UserQuery,
-    AuthQuery,
-))
 
-Mutation = merge_types("Mutation", (
-    UserMutation,
-    AuthMutation,
-))
+@strawberry.type
+class Query:
 
+    @strawberry.field
+    def users(self) -> UserQuery:
+        return UserQuery()
+    
+    @strawberry.field
+    def auth(self) -> AuthQuery:
+        return AuthQuery()
+    
+
+@strawberry.type
+class Mutation:
+
+    @strawberry.field
+    def users(self) -> UserMutation:
+        return UserMutation()
+    
+    @strawberry.field
+    def auth(self) -> AuthMutation:
+        return AuthMutation()
+    
 graphql_schema = strawberry.Schema(
     query=Query,
     mutation=Mutation
